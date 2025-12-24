8d6e8858cb137405d1b00ea96aa6b63f36696090
Nur Hasan Masum <nurhasanmasum@gmail.com>
2025-12-24 14:57:42 +0600

    issue fixed

--- src/utils/sendSMS.ts
+++ src/utils/sendSMS.ts
@@ -2,13 +2,13 @@ import "dotenv/config";
 
 const sendSMS = async (phoneNumber: string, verificationOTP: string) => {
     const messageBody = `Your verification code is ${verificationOTP}`;
-    const accountSid = 'ACbf57a53431bf3db00dc2011280c35c19';
-    const authToken = '31df442714ae41c616b56b6645028190';
+    const accountSid = process.env.TWILIO_ACCOUNT_SID!;
+    const authToken = process.env.TWILIO_AUTH_TOKEN!;
     const client = require('twilio')(accountSid, authToken);
     client.messages
         .create({
             body: messageBody,
-            from: '+18555259062',
+            from: process.env.TWILIO_PHONE_NUMBER!,
             to: phoneNumber
         })
         .then((message: { sid: any; }) => console.log(message.sid));
